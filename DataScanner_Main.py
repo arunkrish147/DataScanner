7196216f371a828e803f01826499eebeccd06bff
arunkrish147 <arunkrish147@gmail.com>
2020-08-09 15:44:14 -0400

    options by specific profitability

--- DataScanner_Main.py
+++ DataScanner_Main.py
@@ -1,13 +1,23 @@
-import keyring as keychain
-
+import keyring as keyring
 import robin_stocks as rh
 
 
+def authenticate():
+    service = "Robinhood"
+    user = "arunkrish147@gmail.com"
+    pwd = keyring.get_password(service, user)
+    login = rh.login(user, pwd)
+    print("Login success")
+    print("**************")
+
+
 def scanner_main():
-    print("Welcome")
+    authenticate()
 
-
-
+    for li in rh.find_options_by_specific_profitability("TSLA", strikePrice="1800", optionType="call",
+                                                        typeProfit="chance_of_profit_short", profitFloor=0.8,
+                                                        profitCeiling=1):
+        print(li)
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #DataScanner
+
+build_user_profile() - provides equity, cash holding, dividend info
+build_holdings() - provides current stock holding info; doesnt provide option info
+find_options_by_specific_profitability
