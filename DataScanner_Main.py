968f72d71b7715b271683034b99bd127cfc50626
arunkrish147 <arunkrish147@gmail.com>
2020-08-15 16:44:56 -0400

    refinement to IV based option search

--- DataScanner_Main.py
+++ DataScanner_Main.py
@@ -15,6 +15,7 @@ rh_options_json = "./rh_options.json"
 ObjectStructure_yaml = "./ObjectStructure.yaml"
 rh_options_expiration_json = "./options_by_expiration.json"
 
+
 def authenticate():
     with open(r'./AccountConfig.yaml') as file:
         config_dict = yaml.full_load(file)
@@ -82,7 +83,7 @@ def master_option_list_refresh():
     print("RH Options List updated!")
 
 
-def scan_options(expiry_date, iv):
+def write_options_to_file(expiry_date):
     with open(rh_options_json, 'r') as infile:
         options_ticker_list = json.load(infile)
 
@@ -90,34 +91,76 @@ def scan_options(expiry_date, iv):
                                                                          expirationDate=expiry_date)
                                    )
     df_option_chain.to_json(r'./options_by_expiration.json')
+    print('Options written to file!')
 
-    #typecasting
-    df_option_chain["implied_volatility"] = pd.to_numeric(df_option_chain["implied_volatility"], downcast="float")
 
-    # filters
-    df_option_chain_filtered = df_option_chain.loc[
-        (df_option_chain['state'] == "active") &
-        (df_option_chain['tradability'] == "tradable") &
-        (df_option_chain['expiration_date'] == expiry_date) &
-        (df_option_chain['implied_volatility'] >= float(iv))
-    ]
+def scan_options(expiry_date, iv):
+    with open('./options_by_expiration.json', 'r') as infile:
+        dic_test = json.load(infile)
+
+    main_df = pd.DataFrame()
+    for key, value in dic_test.items():
+        df_temp = pd.DataFrame(value.values(), columns=[key])
+        main_df = pd.concat([main_df, df_temp], axis=1).reindex(df_temp.index)
+
+    # typecasting
+
+    main_df["volume"] = pd.to_numeric(main_df["volume"], downcast="integer")
+    main_df["open_interest"] = pd.to_numeric(main_df["open_interest"], downcast="integer")
+
+    main_df["strike_price"] = pd.to_numeric(main_df["strike_price"], downcast="float")
+    main_df["chance_of_profit_long"] = pd.to_numeric(main_df["chance_of_profit_long"], downcast="float")
+    main_df["chance_of_profit_short"] = pd.to_numeric(main_df["chance_of_profit_short"], downcast="float")
+    main_df["implied_volatility"] = pd.to_numeric(main_df["implied_volatility"], downcast="float")
+    main_df["delta"] = pd.to_numeric(main_df["delta"], downcast="float")
+    main_df["theta"] = pd.to_numeric(main_df["theta"], downcast="float")
+
+    main_df["ask_price"] = pd.to_numeric(main_df["ask_price"], downcast="float")
+    main_df["ask_size"] = pd.to_numeric(main_df["ask_size"], downcast="float")
+    main_df["bid_price"] = pd.to_numeric(main_df["bid_price"], downcast="float")
+    main_df["bid_size"] = pd.to_numeric(main_df["bid_size"], downcast="float")
+
+    # Row Filters
+    df_option_chain_filtered = main_df.loc[
+        (main_df['state'] == "active") &
+        (main_df['tradability'] == "tradable") &
+        (main_df['expiration_date'] == expiry_date) &
+        (main_df['implied_volatility'] >= float(iv)) &
+        (main_df['volume'] > 999) &
+        (main_df['bid_size'] > 9) &
+        (main_df['ask_size'] > 9)
+        ]
 
     df_option_chain_view = df_option_chain_filtered[get_option_schema()]
-    #df_option_chain_view.sort_values(by=['implied_volatility'], inplace=True, ascending=False)
 
-    pd.set_option("max_columns", None)
-    print(df_option_chain_view.nlargest(100, 'implied_volatility'))
+    # rename columns
+    df_option_chain_view = df_option_chain_view.rename(
+        columns={"chance_of_profit_long": "Call Profit%", "chance_of_profit_short": "Put Profit%",
+                 "implied_volatility": "IV"})
 
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.width", None)
+    pd.set_option("display.max_colwidth", None)
+    pd.options.display.float_format = "{:.2f}".format
 
+    print(" ")
+    print(" OPTIONS SCAN RESULTS")
+    print("---------------------")
+
+    print(df_option_chain_view.nlargest(100, 'IV'))
 
 
 def scanner_main():
-    authenticate()
+    # authenticate()
 
     # master_stock_list_refresh()  # Generates RH stock ticker list and saves to rh_stocks json file
     # master_option_list_refresh() # Generates RH option ticker list and saves to rh_options json file
 
-    scan_options(expiry_date='2020-08-21', iv=0.6)
+    # write_options_to_file(expiry_date='2020-08-21')   # Generates RH option data based on RH options ticker list
+    #                                                   # and writes the data to options_by_expiration file
+
+    scan_options(expiry_date='2020-08-21', iv=0.7)
 
 
 if __name__ == "__main__":
